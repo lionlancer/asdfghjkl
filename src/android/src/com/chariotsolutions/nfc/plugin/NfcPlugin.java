d7a44efc103e8b2aa19e697ed5061c5f3e0739fa
Lionlancer <lionlancer@gmail.com>
2018-03-28 11:17:16 +0800

    asd

--- src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
+++ src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
@@ -1173,9 +1173,9 @@ public class NfcPlugin extends CordovaPlugin implements NfcAdapter.OnNdefPushCom
 						try {
 							
 							response = nfca.transceive(new byte[] {
-									(byte) 0x30, // READ
-									//(byte) 131   // page address Ntag215
-									(byte) 0x83   // page address Ntag215
+									(byte) 0x30,  // READ
+									(byte) 131  // page address Ntag215
+									//(byte) 0x83   // page address Ntag215
 							});
 						
 						}catch(Exception e){
