50e210d517bde749b37933775233c52ee6df0dd5
Lionlancer <lionlancer@gmail.com>
2018-04-03 20:05:31 +0800

    asasd

--- src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
+++ src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
@@ -1214,7 +1214,7 @@ public class NfcPlugin extends CordovaPlugin implements NfcAdapter.OnNdefPushCom
 					try{
 						int start = 4;
 						//int last = 256;
-						int last = 250;
+						int last = 255;
 						response = nfca.transceive(new byte[] {
 								(byte) 0x3A, // FAST_READ
 								(byte) ((4 + start / 4) & 0x0FF),  // first page address
