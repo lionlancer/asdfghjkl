3b73cbdeac6bd8f1bf907b8dc29b88c71251935b
Lionlancer <lionlancer@gmail.com>
2018-04-10 18:54:01 +0800

    more escapes

--- src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
+++ src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
@@ -1178,23 +1178,23 @@ public class NfcPlugin extends CordovaPlugin implements NfcAdapter.OnNdefPushCom
 		
 		Log.d(TAG, "Escaping: " + escapedStr);
 		
-		escapedStr = escapedStr.replaceAll("\\", "\\\\");
+		escapedStr = escapedStr.replaceAll("\\\\", "\\\\\\\\");
 		Log.d(TAG, "Replacing \\\\: " + escapedStr);
 		
 		//escapedStr = escapedStr.replaceAll("\/", "\\/");
-		escapedStr = escapedStr.replaceAll("\b", "\\b");
+		escapedStr = escapedStr.replaceAll("\\b", "\\\\b");
 		Log.d(TAG, "Replacing \\b: " + escapedStr);
 		
-		escapedStr = escapedStr.replaceAll("\f", "\\f");
+		escapedStr = escapedStr.replaceAll("\\f", "\\\\f");
 		Log.d(TAG, "Replacing \\f: " + escapedStr);
 		
-		escapedStr = escapedStr.replaceAll("\n", "\\n");
+		escapedStr = escapedStr.replaceAll("\\n", "\\\\n");
 		Log.d(TAG, "Replacing \\n: " + escapedStr);
 		
-		escapedStr = escapedStr.replaceAll("\r", "\\r");
+		escapedStr = escapedStr.replaceAll("\\r", "\\\\r");
 		Log.d(TAG, "Replacing \\r: " + escapedStr);
 		
-		escapedStr = escapedStr.replaceAll("\t", "\\t");
+		escapedStr = escapedStr.replaceAll("\\t", "\\\\t");
 		Log.d(TAG, "Replacing \\t: " + escapedStr);
 		
 		return escapedStr;		
